89f6a1e7a902c2de38290291fe5ecc63b537404e
vitordefreyn-del <vitor_defreyn@estudante.sesisenai.org.br>
2026-04-30 17:07:06 -0300

    codigo atualizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
+const [pokemons,setPokemons] = useState ({});
+const [loading, setLoading] = useState (true);
+const [error, setError] = useState (false);
+
+useEffect (() => {
+  const getData = async () => {
+    try {
+      const res = await 
+      axios.get('https://Pokeapi.co/api/v2/pokemon/rowlet');
+      setPokemons(res.data);
+      console.log('Success:', res.data);
+      setLoading(false); 
+
+    }
+    catch (err) {
+      console.error("Erro ao carregar API", err);
+      
+      setLoading(false)
+
+      setError(true)
+    }
+  };
+  getData();
+}, []);
+
+
+  if (loading) return <div className="loader">Carregando Pokemons. . . ></div>;
+  if (error) return <div className="error">ocorreu um erro inesperado></div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "container-background"  >
+    <div className= "App">
+    <h1>poke card</h1>
+    <div className="container-carta">
+    <div className="pokemon-card">
+    <h3>{pokemons.name}</h3>
+    <h2 className='tipo-pokemon1'>{pokemons.types[0].type.name}</h2>
+    <img src={pokemons.sprites.front_default} alt={pokemons.name} style={{ width: '180px' }} />
+    <h3>{pokemons.stats[0].stat.name + ' ' + 
+
+    pokemons.stats[0].base_stat + ' ' + pokemons.stats[1].stat.name + ' ' + 
+    
+    pokemons.stats[1].base_stat }</h3>
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
+
 
 export default App;
